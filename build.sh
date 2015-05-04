a6d2bb84ba583e9dd3ea0f230cf70e3ebae79b7e
Mickael Guene <mickael.guene@st.com>
2015-05-04 17:04:32 +0200

    clean script for sid

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash -ex
 
-VERSION=$1
-if [[ -z "$VERSION" ]]; then
-    echo "empty"
-else
-    echo $VERSION
-fi
-
 #get script location
 SCRIPTDIR=`dirname $0`
 SCRIPTDIR=`(cd $SCRIPTDIR ; pwd)`
 
-. ${SCRIPTDIR}/build_common.sh
+. ${SCRIPTDIR}/build_common.sh sid
 
